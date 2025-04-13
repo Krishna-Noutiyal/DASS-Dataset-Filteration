6726bd47916d1213a153918dc705bcabfb577565
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-04-13 12:35:53 +0530

    Before categorizing with major_mapv2

--- Filtering/major_grouping.py
+++ Filtering/major_grouping.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from Filtering.map import major_mapv1
+from subprocess import run
+from Filtering.map import major_mapv1, major_mapv2
 
 df = pd.read_csv("./filtered data/dass_filtered_major_spec_rem.csv")  # Load the dataset
 
@@ -8,7 +9,8 @@ def categorize_major(major):
     if pd.isnull(major):
         return "Others"
     major_lower = major.lower()
-    for category, keywords in major_mapv1.items():
+    # for category, keywords in major_mapv1.items():
+    for category, keywords in major_mapv2.items():
         if any(keyword in major_lower for keyword in keywords):
             return category
     return "Others"
@@ -17,4 +19,8 @@ df['major_category'] = df['major'].apply(categorize_major)
 
 
 
-df.to_csv("./filtered data/categorized_data.csv", index=False)
+# df.to_csv("./filtered data/categorized_datav1.csv", index=False)
+df.to_csv("./filtered data/categorized_datav2.csv", index=False)
+
+# Run the verification script
+run(["python", "./verify_map.py"])
